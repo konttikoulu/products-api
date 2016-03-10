dd1f38c33c1850ef144dd858aa7a8b3497b259fc
Tero Ahonen <tero@calevala.com>
2016-03-10 14:30:15 +0200

    Version upgrade

--- src/main/java/com/tahonen/ostesting/productservice/JaxRsActivator.java
+++ src/main/java/com/tahonen/ostesting/productservice/JaxRsActivator.java
@@ -3,7 +3,7 @@ package com.tahonen.ostesting.productservice;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/v1")
+@ApplicationPath("/v2")
 public class JaxRsActivator extends Application {
    /* class body intentionally left blank */
 }
